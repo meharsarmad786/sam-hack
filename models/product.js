9ef47e8e2cd794d2c0c1832288e84fd937a792b0
meharsarmad786 <72754417+meharsarmad786@users.noreply.github.com>
2020-12-03 08:35:35 -0800

    Revert "add this"
    This reverts commit 4f36ac7837904a66d0df541401ead90703b8113f.

--- models/product.js
+++
@@ -1,9 +0,0 @@
-var mongoose=require("mongoose");
-var productSchema =mongoose.Schema({
-    name:String,
-    contact:String,
-    city:String,
-    service:String,
-});
-const Product=mongoose.model("Product",productSchema);
-module.exports=Product;
